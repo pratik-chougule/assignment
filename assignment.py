8e2a716dee8a9a8c7921be1ddd60dc89e7adafd3
Pratik <pratikchougule1994@gmail.com>
2023-11-13 16:34:59 +0530

    "first"

--- assignment.py
+++ assignment.py
@@ -1,5 +1,6 @@
 import io
 import os
+import shutil
 import xml
 
 from fastapi import FastAPI
@@ -8,7 +9,7 @@ from pydantic_xml import BaseXmlModel
 
 from datetime import date
 import requests
-from starlette.responses import FileResponse
+from starlette.responses import FileResponse, StreamingResponse
 
 
 class bank_account_holder_deatils_model(BaseModel):
@@ -29,18 +30,31 @@ class bank_account_holder_deatils_model(BaseModel):
 from fastapi import FastAPI, Response, Request, HTTPException, UploadFile
 
 app = FastAPI()
-BASE_DIR =  os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 Upload_DIR = os.path.join(BASE_DIR, "uploads")
-
+import pandas as pd
+from io import BytesIO
 @app.post("/submit")
-async def submit(file: UploadFile, response_model=bank_account_holder_deatils_model):
-    if file.conetent_type !="application/xml":
-        raise HTTPException(400, detail="Invalid file type")
-    else:
-        xml_data = xml.load(file.file.read())
-        new_filename = '{} {}.xls'.format(os.path.splitext(file.filename)[0])
+async def submit(file: UploadFile):
+    content_type = file.content_type
+    upload_dir = os.path.join(os.getcwd(), "uploads")
 
-        save_file_path = os.path.join(Upload_DIR, new_filename)
-        with open(save_file_path, "w") as f:
-            xls.dump(xml_data)
-    return FileResponse(path=save_file_path, media_type="application/xls", filename=new_filename)
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+
+    dest = os.path.join(upload_dir, file.filename)
+    if content_type != "text/xml":
+        raise HTTPException(status_code=400, detail="invalid file type")
+    df = pd.DataFrame(file.file)
+    # with open(dest, "wb") as buffer:
+    #     shutil.copyfileobj(file.file, buffer)
+
+    buffer = BytesIO()
+    # return FileResponse(path=dest, filename=file.filename)
+    with pd.ExcelWriter(buffer) as writer:
+        df.to_excel(writer, index=False)
+    return StreamingResponse(
+        BytesIO(buffer.getvalue()),
+        media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        headers={"Content-Disposition": f"attachment; filename=input.xls"}
+    )
